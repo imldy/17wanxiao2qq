1db6d03a9baed018e503c5337dfac38b4167f4ab
imldy <im.ldy@outlook.com>
2021-11-10 12:54:49 +0800

    完善Task，增加Dest、Crowd、Account、WX_Account、Remind类

--- beans.py
+++ beans.py
@@ -1,8 +1,13 @@
 class Task():
     def __init__(self, id, name, remind_text):
-        self.id = id
-        self.name = name
-        self.remind_text = remind_text
+        self.id = id  # 任务ID
+        self.name = name  # 任务名
+        self.remind_text = remind_text  # 任务提醒语句，描述语句
+        self.remind_type = None  # 提醒类型，简单的文本|需要at的
+        self.dest = None  # 任务目的地，某个群或者某个QQ账号
+        self.involve_crowd = None  # 消息中涉及到的人
+        self.date = None  # 标准日期
+        self.date_offset = None  # 标准日期的偏移量，+1代表提前一天
 
 
 class Student():
@@ -14,3 +19,42 @@ class Student():
             self.ignore = False
         else:
             self.ignore = True
+
+
+class Dest():
+    GROUP = "GROUP"
+    PERSON = "PERSON"
+
+    def __init__(self, tag: str):
+        self.tag: str = tag
+        self.type: str = None
+        self.no: str = None
+        self.nos: str = None
+        self.email: str = None
+
+
+class Crowd():
+    CLASS_ALL = "class-all"
+    WX_NO_HEALTH_CHECKIN = "wx-no-health-checkin"
+    BOY_DORMITORY_CLEANER = "boy-dormitory-cleaner"
+    GIRL_DORMITORY_CLEANER = "girl-dormitory-cleaner"
+    CLASSROOM_CLEANER = "classroom-cleaner"
+
+    def __init__(self):
+        self.tag: str = None
+        self.name: str = None
+
+
+class Account():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+class WX_Account(Account):
+    pass
+
+
+class Remind():
+    PLAIN = "plain"
+    NEED_AT = "Need At"
