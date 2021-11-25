26ac8be5ac0c5c9db7609e99b2c60994bb79a139
imldy <im.ldy@outlook.com>
2021-11-25 23:57:40 +0800

    增加获取对象的方法WX2QQService、TaskDao、DestDao

--- factory.py
+++ factory.py
@@ -1,4 +1,6 @@
+from dao import TaskDao, DestDao
 from net_api import QQBot
+from wx2qq import WX2QQService
 
 
 class BeansFactory():
@@ -8,6 +10,8 @@ class BeansFactory():
     def getBeans(self, beans_name: str):
         if beans_name.lower() == "QQBot".lower():
             return self.getQQBot(self.conf)
+        elif beans_name.lower() == "WX2QQService".lower():
+            return self.getWX2QQService(self.conf)
 
     def getQQBot(self, conf=None):
         if conf == None:
@@ -16,3 +20,15 @@ class BeansFactory():
         qqbot.verify()
         qqbot.bind()
         return qqbot
+
+    def getWX2QQService(self, conf=None):
+        wx2QQService = WX2QQService(conf)
+        return wx2QQService
+
+    def getTaskDao(self, conf=None):
+        taskDao = TaskDao(conf)
+        return taskDao
+
+    def getDestDao(self, conf=None):
+        destDao = DestDao(conf)
+        return destDao
